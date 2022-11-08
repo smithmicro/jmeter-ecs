709f04acc87777ae8acfe22665910c7f2768d38d
Martin Prikryl <martin.prikryl@gmail.com>
2022-11-08 12:47:45 +0100

    Add support for multiple parallel .jmx plans

--- jmeter/entrypoint.sh
+++ jmeter/entrypoint.sh
@@ -5,6 +5,10 @@
 # any .jmx file passed in the command line we act as 'Gru'
 if [ ${1##*.} = 'jmx' ]; then
 
+  if [ "$MINION_COUNT" = '' ]; then
+    echo "MINION_COUNT must be specified - a command separated list of minion counts of the same length as list of .jmx files in 1st command line argument"
+    exit 1
+  fi
   if [ "$MINION_HOSTS" = '' ]; then
     echo "MINION_HOSTS must be specified - a command separated list of hostnames or IP addresses"
     exit 1
@@ -28,14 +32,43 @@ if [ ${1##*.} = 'jmx' ]; then
   sed -i 's/-Xms1g -Xmx1g -XX:MaxMetaspaceSize=256m//' $JMETER_HOME/bin/jmeter
 
   # run jmeter in client (gru) mode
-  exec jmeter -n $JMETER_FLAGS \
-    -R $MINION_HOSTS \
-    -Dclient.rmi.localport=51000 \
-    -Dserver.rmi.ssl.disable=true \
-    -Djava.rmi.server.hostname=${PUBLIC_HOSTNAME} \
-    -l $RESULTS_LOG \
-    -t $1 \
-    -e -o /logs/report
+  # run jmeter in client (gru) mode for all testplans
+  TEMP_INPUT_JMX=$1,
+  TEMP_MINION_COUNT=$MINION_COUNT,
+  TEMP_MINION_HOSTS=$MINION_HOSTS
+
+  while [ "$TEMP_INPUT_JMX" ]
+  do
+    INPUT_JMX_FOR_TESTPLAN=${TEMP_INPUT_JMX%%,*}
+    MINION_COUNT_FOR_TESTPLAN=${TEMP_MINION_COUNT%%,*}
+    MINION_HOSTS_FOR_TESTPLAN=$(echo $TEMP_MINION_HOSTS | cut -f1-$MINION_COUNT_FOR_TESTPLAN -d,)
+    PORT_FOR_TESTPLAN=$((51000 + ${#TEMP_INPUT_JMX}))
+    NAME_FOR_TESTPLAN=$(basename $INPUT_JMX_FOR_TESTPLAN .jmx)
+    echo MINION_COUNT_FOR_TESTPLAN $MINION_COUNT_FOR_TESTPLAN
+    echo TEMP_MINION_HOSTS $TEMP_MINION_HOSTS
+
+    jmeter -n $JMETER_FLAGS \
+      -R $MINION_HOSTS_FOR_TESTPLAN \
+      -Dclient.rmi.localport=$PORT_FOR_TESTPLAN \
+      -Dserver.rmi.ssl.disable=true \
+      -Djava.rmi.server.hostname=${PUBLIC_HOSTNAME} \
+      -l $NAME_FOR_TESTPLAN.jtl \
+      -j $NAME_FOR_TESTPLAN.log \
+      -t $INPUT_JMX_FOR_TESTPLAN \
+      -e -o /logs/$NAME_FOR_TESTPLAN &
+
+    TEMP_INPUT_JMX=${TEMP_INPUT_JMX#*,}
+    TEMP_MINION_COUNT=${TEMP_MINION_COUNT#*,}
+    TEMP_MINION_HOSTS=$(echo $TEMP_MINION_HOSTS | cut -f$(($MINION_COUNT_FOR_TESTPLAN + 1))- -d,)
+  done
+
+  # wait for jmeter processes to finish
+  while pgrep jmeter > /dev/null
+  do
+    sleep 20
+  done
+
+  exit
 
 fi
 
